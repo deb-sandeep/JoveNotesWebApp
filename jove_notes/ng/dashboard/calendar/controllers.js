5704af0bb579d93b0ec083d16579dca0bc3a763d
Sandeep Deb <deb.sandeep@gmail.com>
2017-04-05 22:03:59 +0530

    Calendar client done.. server integration pending

--- jove_notes/ng/dashboard/calendar/controllers.js
+++ jove_notes/ng/dashboard/calendar/controllers.js
@@ -1,8 +1,70 @@
 dashboardApp.controller( 'CalendarController', function( $scope, $http, $sce ) {
 
 // ---------------- Constants and inner class definition -----------------------
+function Event() {
+    this.type = "" ;
+    this.subject = "" ;
+    this.title = "" ;
+    this.startsAt = "" ;
+    this.color = {
+        primary: "",
+        secondary: "#C1C0C0"
+    } ;
+    this.draggable = false ;
+    this.resizable = false ;
+    this.incrementsBadgeTotal = true ;
+    this.allDay = true ;
+
+    this.typeOld     = "" ;
+    this.subjectOld  = "" ;
+    this.titleOld    = "" ;
+    this.startsAtOld = "" ;
+    this.colorOld    = {
+        primary: "",
+    } ;
+
+    this.createSnapshot = function() {
+        this.typeOld          = this.type          ;
+        this.subjectOld       = this.subject       ;
+        this.titleOld         = this.title         ;
+        this.startsAtOld      = this.startsAt      ;
+        this.colorOld.primary = this.color.primary ;
+    }
+
+    this.isDirty = function() {
+        if( this.typeOld          != this.type          ){ return true; }
+        if( this.subjectOld       != this.subject       ){ return true; }
+        if( this.titleOld         != this.title         ){ return true; }
+        if( this.startsAtOld      != this.startsAt      ){ return true; }
+        if( this.colorOld.primary != this.color.primary ){ return true; }
+        return false ;
+    }
+
+    this.subjectChanged = function() {
+        if( this.type == "Exam" ) {
+            this.title = this.subject + " Exam" ;
+        }
+    }
+}
 
 // ---------------- Local variables --------------------------------------------
+var e1 = new Event() ;
+var e2 = new Event() ;
+
+e1.type          = 'Exam' ;
+e1.subject       = 'Literature' ;
+e1.title         = 'English Literature' ;
+e1.startsAt      = new Date(2017,3,6) ;
+e1.color.primary = '#e3bc08' ;
+
+e2.type          = 'General' ;
+e2.subject       = 'Hindi' ;
+e2.title         = 'Naya Rasta Test' ;
+e2.startsAt      = new Date(2017,3,7) ;
+e2.color.primary = '#E33D08' ;
+
+e1.createSnapshot() ;
+e2.createSnapshot() ;
 
 // ---------------- Controller variables ---------------------------------------
 $scope.$parent.pageTitle     = "Event Calendar" ;
@@ -16,36 +78,15 @@ $scope.calendar = {
     editEvents   : false,
     selectedDate : new Date(),
     editAllEvents: false,
-    events       : [
-                      {
-                        type: 'Exam',
-                        subject: 'Literature',
-                        title: 'English Literature',
-                        startsAt: new Date(2017,3,6), // A javascript date object for when the event starts
-                        color: {
-                          primary: '#e3bc08', // the primary event color (should be darker than secondary)
-                          secondary: '#C1C0C0' // the secondary event color (should be lighter than primary)
-                        },
-                        draggable: false,
-                        resizable: false,
-                        incrementsBadgeTotal: true,
-                        allDay: true
-                      },
-                      {
-                        type: 'General',
-                        subject: 'Hindi',
-                        title: 'Hindi',
-                        startsAt: new Date(2017,3,7),
-                        color: {
-                          primary: '#E33D08',
-                          secondary: '#C1C0C0'
-                        },
-                        draggable: false,
-                        resizable: false,
-                        incrementsBadgeTotal: true,
-                        allDay: true
-                      }
-                    ],
+    events       : [e1, e2],
+    possibleSubjects : [
+        'English',
+        'Hindi',
+        'Marathi',
+        'Mathematics',
+        'Hindi - Naya Rasta',
+        'Literature'
+    ],
     eventsForEditing : []
 }
 
@@ -73,10 +114,6 @@ $scope.timespanClicked = function(date, cell) {
     recomputeEditableEvents() ;
 }
 
-$scope.addNewEvent = function() {
-
-}
-
 $scope.toggleCalendar = function( $event, field, event ) {
     $event.preventDefault();
     $event.stopPropagation();
@@ -94,6 +131,29 @@ $scope.getColorPickerStyle = function( event ) {
         'background-color':event.color.primary,
         'color':event.color.primary 
     } ;
+}
+
+$scope.saveEvent = function( event ) {
+    event.createSnapshot() ;
+    recomputeEditableEvents() ;
+    // TODO: Save it to the server
+}
+
+$scope.deleteEvent = function( index ) {
+    var event = $scope.calendar.events.splice( index, 1 ) ;
+    recomputeEditableEvents() ;
+    // TODO: Delete it from the server
+}
+
+$scope.newEvent = function() {
+    var e           = new Event() ;
+    e.type          = "General" ;
+    e.title         = "<Event Title>" ;
+    e.startsAt      = $scope.calendar.selectedDate ;
+    e.color.primary = "#7981FA" ;
+
+    $scope.calendar.events.push( e ) ;
+    recomputeEditableEvents() ;
 }
 
 // ---------------- Private calls -----------------------------------------------
