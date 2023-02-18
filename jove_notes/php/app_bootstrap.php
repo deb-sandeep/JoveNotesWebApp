ca44ad0e95f248ad856e6137699f5090a5ca142d
Sandeep Deb <deb.sandeep@gmail.com>
2023-02-18 10:56:36 +0530

    Integrated with PHPStorm

--- jove_notes/php/app_bootstrap.php
+++ jove_notes/php/app_bootstrap.php
@@ -4,10 +4,10 @@ require_once( $_SERVER['DOCUMENT_ROOT']."/lib-app/php/page_preprocessor.php" ) ;
 require_once( DOCUMENT_ROOT . "/lib-app/php/services/authorization_service.php" ) ;
 require_once( DOCUMENT_ROOT . "/apps/_common/php/common_app_config.php" ) ;
 
-define( "APP_NAME", "jove_notes" ) ;
+const APP_NAME = "jove_notes";
 
-define( "APP_LOGO_PATH",         "/apps/" . APP_NAME . "/media/images/logo_for_navbar.png" ) ;
-define( "APP_MAIN_CSS_PATH",     "/apps/" . APP_NAME . "/style/main.css" ) ;
+const APP_LOGO_PATH = "/apps/" . APP_NAME . "/media/images/logo_for_navbar.png";
+const APP_MAIN_CSS_PATH = "/apps/" . APP_NAME . "/style/main.css";
 
 if( !Authorizer::isUserInRole( "JN_USER" ) ) {
 	
